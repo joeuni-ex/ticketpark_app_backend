110070de0e0506ab496ba2b9fa61f36343d4cf08
joeuni <dmsdl8917@gmail.com>
2024-06-03 15:15:07 +0900

    Initial commit

---
+++ src/main/java/org/zerock/ticketapiserver/TicketapiserverApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.ticketapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketapiserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketapiserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerock/ticketapiserver/TicketapiserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.ticketapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketapiserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
