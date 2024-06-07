87bc8b25a27b9b11c6f9be6c048658963d126606
joeuni <dmsdl8917@gmail.com>
2024-06-07 17:16:48 +0900

    로그인 정보 json 타입으로 리턴

--- src/main/java/org/zerock/ticketapiserver/config/CustomSecurityConfig.java
+++ src/main/java/org/zerock/ticketapiserver/config/CustomSecurityConfig.java
@@ -5,12 +5,14 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.zerock.ticketapiserver.handler.APILoginSuccessHandler;
 
 import java.util.Arrays;
 
@@ -28,12 +30,21 @@ public class CustomSecurityConfig {
         http.cors(httpSecurityCorsConfigurer -> {
             httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource());
         });
+
+        //세션 만들지 않게 설정하기
+        http.sessionManagement(httpSecuritySessionManagementConfigurer -> {
+            httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        });
+
+
+
         //csrf
         http.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
 
         //로그인 확인 테스트
         http.formLogin(config -> {
             config.loginPage("/api/member/login");
+            config.successHandler(new APILoginSuccessHandler());
         });
 
 
