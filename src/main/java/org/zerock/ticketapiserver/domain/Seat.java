bc32955725a92cc3de75deb56da46a268348d66d
joeuni <dmsdl8917@gmail.com>
2024-06-11 19:12:37 +0900

    dtoToEntity , entityToDto 및 좌석 변경 기능 구현

--- src/main/java/org/zerock/ticketapiserver/domain/Seat.java
+++ src/main/java/org/zerock/ticketapiserver/domain/Seat.java
@@ -29,4 +29,24 @@ public class Seat {
     private Goods goods;
 
 
+    //좌석클래스 변경
+    public void changeSeatClass (String seatClass){
+        this.seatClass =seatClass;
+    }
+
+
+    //좌석번호 변경
+    public void changeSeatNumber (int seatNumber){
+        this.seatNumber =seatNumber;
+    }
+
+
+
+    //좌석가격 변경
+    public void changeSeatPrice (int price){
+        this.price =price;
+    }
+
+
+
 }
--- src/main/java/org/zerock/ticketapiserver/service/SeatServiceImpl.java
+++ src/main/java/org/zerock/ticketapiserver/service/SeatServiceImpl.java
@@ -4,9 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.zerock.ticketapiserver.domain.Goods;
+import org.zerock.ticketapiserver.domain.Member;
+import org.zerock.ticketapiserver.domain.Reservation;
 import org.zerock.ticketapiserver.domain.Seat;
 import org.zerock.ticketapiserver.dto.ReservationDTO;
 import org.zerock.ticketapiserver.repository.SeatRepository;
+
+import java.util.Optional;
 
 @Service
 @Log4j2
@@ -18,22 +22,60 @@ public class SeatServiceImpl implements SeatService {
     //추가
     @Override
     public Long register(ReservationDTO reservationDTO) {
-        Goods goods = Goods.builder().gno(reservationDTO.getGno()).build();
-
-        Seat seat = Seat.builder()
-                .goods(goods)
-                .seatClass(reservationDTO.getSeatClass())
-                .seatNumber(reservationDTO.getSeatNumber())
-                .price(reservationDTO.getPrice())
-                .build();
-
-        log.info("------------------------------");
-        log.info(seat);
-
+        Seat seat = dtoToEntity(reservationDTO);
 
         Long sno = seatRepository.save(seat).getSno();
 
         return sno;
     }
 
+
+    //좌석변경
+    @Override
+    public Long modify(ReservationDTO reservationDTO) {
+
+        //조회
+        Optional<Seat> result = seatRepository.findById(reservationDTO.getSno());
+
+        Seat seat = result.orElseThrow();
+        //변경 내용 반영
+        seat.changeSeatClass(reservationDTO.getSeatClass());
+        seat.changeSeatNumber(reservationDTO.getSeatNumber());
+        seat.changeSeatPrice(reservationDTO.getPrice());
+
+        Long sno = seatRepository.save(seat).getSno();
+
+        return sno;
+    }
+
+
+
+
+
+    // DTO -> 엔티티 변환 (저장 시 사용)
+    private Seat dtoToEntity(ReservationDTO seatDTO) {
+        Goods goods = Goods.builder().gno(seatDTO.getGno()).build();
+
+        Seat seat = Seat.builder()
+                .goods(goods)
+                .seatClass(seatDTO.getSeatClass())
+                .seatNumber(seatDTO.getSeatNumber())
+                .price(seatDTO.getPrice())
+                .build();
+
+        return seat;
+    }
+
+    // 엔티티 -> DTO 변환 (조회 시 사용)
+    private ReservationDTO entityToDto(Seat seat) {
+        ReservationDTO reservationDTO = ReservationDTO.builder()
+                .sno(seat.getSno())
+                .gno(seat.getGoods().getGno())
+                .seatClass(seat.getSeatClass())
+                .seatNumber(seat.getSeatNumber())
+                .price(seat.getPrice())
+                .build();
+
+        return reservationDTO;
+    }
 }
